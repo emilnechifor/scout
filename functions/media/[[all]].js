383057bd40b6dee2a70c0236f5fc663bbe2b50a7
emil nechifor <emilnechifor@live.com>
2024-11-18 23:25:26 +0100

    js not loading

--- functions/media/[[all]].js
+++ functions/media/[[all]].js
@@ -1,6 +1,6 @@
 export async function onRequestGet(ctx) {
-  const path = new URL(ctx.request.url).pathname.replace("/scout-test/", "");
-  const file = await ctx.env.scout-test.get(path);
+  const path = new URL(ctx.request.url).pathname.replace("/media/", "");
+  const file = await ctx.env.MEDIA.get(path);
   if (!file) return new Response(null, { status: 404 });
   return new Response(file.body, {
     headers: { "Content-Type": file.httpMetadata.contentType },
