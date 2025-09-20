ecdfe1414ac03a1b63a9f846d17054bdff40126f
emilnechifor <emilnechifor@gmail.com>
2025-09-20 14:36:24 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -79,7 +79,7 @@ document.addEventListener('DOMContentLoaded', function() {
         link.addEventListener("click", function(event) {
             
             const href = this.getAttribute('href');
-            if (href === "/") {
+            if (href.includes('/')) {
                 //showSection("home");
             } else {
                 event.preventDefault();
